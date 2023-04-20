efa80a7717074691a0712a86ef06e81f705e4eb9
Leonid Medovyy <leonid@storypro.io>
2023-04-20 11:18:05 -0700

    disabling unused components

--- app/jobs/enchance_job.rb
+++ app/jobs/enchance_job.rb
@@ -15,17 +15,17 @@ class EnchanceJob < ApplicationJob
     # to avoid the rate limit errors
     posts[0..20].each do |post|
       url = post.fields['URL']
-      hashtag = post.fields['Hashtags'].last
-      image = pexels.find(keyword: hashtag)
+      category = post.fields['Category'].last
 
-      generated_screenshot = screenshot.render(url: url)
-      social_card = placid.generate(title: post.fields['Text'],
-                                    category: hashtag,
-                                    image: image)
-
-      post['Screenshot'] = [{ url: generated_screenshot }] if generated_screenshot
-      post['Social'] = [{ url: social_card }]
-      post['Processed'] = true
+      # image = pexels.find(keyword: category)
+      # generated_screenshot = screenshot.render(url: url)
+      # social_card = placid.generate(title: post.fields['Text'],
+      #                               category: hashtag,
+      #                               image: image)
+      #
+      # post['Screenshot'] = [{ url: generated_screenshot }] if generated_screenshot
+      # post['Social'] = [{ url: social_card }]
+      # post['Processed'] = true
 
       post.save
     end
